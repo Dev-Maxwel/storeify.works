46631c839fa08e41dc9bd194261ff27bbd752e20
devmaxwell <developermaxwellkaranja@gmail.com>
2023-11-25 14:39:54 +0300

    imagecontrols model modified

--- dashboard/models.py
+++ dashboard/models.py
@@ -8,7 +8,9 @@ class emailMarketing(models.Model):
 
     def __str__(self):
         return self.email
-    
+    class Meta:
+        verbose_name_plural = 'emailMarketing'
+        
 class imageControls(models.Model):
     image1 = models.ImageField(upload_to="storeifyworks_cdn", height_field=None, width_field=None, max_length=None, blank=True, null=True) 
     alt1 = models.TextField(blank=True, null=True, max_length=120)  
@@ -34,4 +36,7 @@ class imageControls(models.Model):
     alt6 = models.TextField(blank=True, null=True, max_length=120)  
     description6 = models.TextField(blank=True, null=True) 
 
-    
+    def __str__(self):
+        return self.alt1
+    class Meta:
+        verbose_name_plural = 'imageControls'
