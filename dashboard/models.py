f8ea0a3afb818f28e46829219c50394ec29f0091
devmaxwell <developermaxwellkaranja@gmail.com>
2023-12-07 23:41:38 +0300

    image models paths changed

--- dashboard/models.py
+++ dashboard/models.py
@@ -12,27 +12,27 @@ class emailMarketing(models.Model):
         verbose_name_plural = 'emailMarketing'
         
 class imageControls(models.Model):
-    image1 = models.ImageField(upload_to="testing/", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
+    image1 = models.ImageField(upload_to="dashboard", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
     alt1 = models.TextField(blank=True, null=True, max_length=120, default="")  
     description1 = models.TextField(blank=True, null=True, default="") 
     
-    image2 = models.ImageField(upload_to="testing/", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
+    image2 = models.ImageField(upload_to="dashboard", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
     alt2 = models.TextField(blank=True, null=True, max_length=120, default="")  
     description2 = models.TextField(blank=True, null=True, default="") 
     
-    image3 = models.ImageField(upload_to="testing/", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
+    image3 = models.ImageField(upload_to="dashboard", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
     alt3 = models.TextField(blank=True, null=True, max_length=120, default="")  
     description3 = models.TextField(blank=True, null=True, default="") 
     
-    image4 = models.ImageField(upload_to="testing/", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
+    image4 = models.ImageField(upload_to="dashboard", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
     alt4 = models.TextField(blank=True, null=True, max_length=120, default="")  
     description4 = models.TextField(blank=True, null=True, default="") 
     
-    image5 = models.ImageField(upload_to="testing/", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
+    image5 = models.ImageField(upload_to="dashboard", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
     alt5 = models.TextField(blank=True, null=True, max_length=120, default="")  
     description5 = models.TextField(blank=True, null=True, default="") 
     
-    image6 = models.ImageField(upload_to="testing/", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
+    image6 = models.ImageField(upload_to="dashboard", height_field=None, width_field=None, max_length=None, blank=True, null=True, default="") 
     alt6 = models.TextField(blank=True, null=True, max_length=120, default="")  
     description6 = models.TextField(blank=True, null=True, default="") 
 
